9b29bf27d82dd05f561f63a52d44a974c0106400
José Miguel Tajuelo Garrigós <josemigueltajuelo@gmail.com>
2020-02-19 19:32:20 +0100

    Added main menu scene

---
+++ Assets/Scripts/CustomSceneManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CustomSceneManager : MonoBehaviour
+{	public void LoadMenu() {
+		SceneManager.LoadScene(MainMenuScene);
+	}
+
+	public void LoadLevel() {
+		SceneManager.LoadScene(LevelScene);
+	}
+
+	public void ExitGame() {
+		Application.Quit();
+	}
+
+	//[SerializeField]
+	private string MainMenuScene = "MainMenuScene";
+
+	//[SerializeField]
+	private string LevelScene = "LevelScene";
+}
