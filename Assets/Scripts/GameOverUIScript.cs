f01aebe4b305c9cdbffb6a27071646403e71c8f1
José Miguel Tajuelo Garrigós <josemigueltajuelo@gmail.com>
2020-02-19 21:29:18 +0100

    Added wrapper scripts (Main menu and Game over) that use the singleton Scene Manager for their buttons

---
+++ Assets/Scripts/GameOverUIScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOverUIScript : MonoBehaviour
+{
+	void Awake() {
+		m_RetryButton.onClick.AddListener(delegate { CustomSceneManager.sceneManager.LoadLevel(); });
+		m_MenuButton.onClick.AddListener(delegate { CustomSceneManager.sceneManager.LoadMenu(); });
+	}
+
+	[SerializeField]
+	private Button m_RetryButton;
+
+	[SerializeField]
+	private Button m_MenuButton;
+}
---
+++ Assets/Scripts/MainMenuUIScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuUIScript : MonoBehaviour
+{
+	void Awake() {
+		m_PlayButton.onClick.AddListener(delegate { CustomSceneManager.sceneManager.LoadLevel(); });
+		m_ExitButton.onClick.AddListener(delegate { CustomSceneManager.sceneManager.ExitGame(); });
+	}
+
+	[SerializeField]
+	private Button m_PlayButton;
+
+	[SerializeField]
+	private Button m_ExitButton;
+}
