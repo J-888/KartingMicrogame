ea98d7d15d149bb920ad1d44fc62886340683484
José Miguel Tajuelo Garrigós <josemigueltajuelo@gmail.com>
2020-02-21 19:19:37 +0100

    Added record time to main menu

--- Assets/Scripts/MainMenuUIScript.cs
+++ Assets/Scripts/MainMenuUIScript.cs
@@ -2,12 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using KartGame.Track;
 
 public class MainMenuUIScript : MonoBehaviour
 {
 	void Awake() {
 		m_PlayButton.onClick.AddListener(delegate { CustomSceneManager.sceneManager.LoadLevel(); });
 		m_ExitButton.onClick.AddListener(delegate { CustomSceneManager.sceneManager.ExitGame(); });
+
+		DisplayRecord();
+	}
+
+	void DisplayRecord() {
+		TrackRecord tr = TrackRecord.Load(trackName, 1);
+		if(tr != null && tr.time < float.PositiveInfinity) {
+			m_RecordText.gameObject.SetActive(true);
+			m_RecordText.text = string.Format(m_RecordText.text, tr.time);
+		}
+		else {
+			m_RecordText.gameObject.SetActive(false);
+		}
 	}
 
 	[SerializeField]
@@ -15,4 +29,9 @@ public class MainMenuUIScript : MonoBehaviour
 
 	[SerializeField]
 	private Button m_ExitButton;
+
+	[SerializeField]
+	private Text m_RecordText;
+
+	private string trackName = "ArtTest";
 }
