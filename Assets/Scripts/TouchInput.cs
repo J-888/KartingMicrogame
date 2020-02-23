b4e5cc3d76e87d615576126a59a2171385972e9e
José Miguel Tajuelo Garrigós <josemigueltajuelo@gmail.com>
2020-02-23 17:38:24 +0100

    Hop/Drift support for Touch input

--- Assets/Scripts/TouchInput.cs
+++ Assets/Scripts/TouchInput.cs
@@ -39,14 +39,18 @@ namespace KartGame.KartSystems
 
 		void Update() {
 
-			bool leftTouch, rightTouch;
-			GetTouchInputs(out leftTouch, out rightTouch);
+			bool leftTouch, rightTouch, doubletapHeld, doubletapBegan;
+			float doubletapAxis;
+			GetTouchInputs(out leftTouch, out rightTouch, out doubletapHeld, out doubletapBegan, out doubletapAxis);
 
-
-			if(leftTouch && rightTouch) {
+			if(doubletapHeld) {	// hop
+				m_Acceleration = 1f;
+				m_Steering = doubletapAxis;
+			}
+			else if(leftTouch && rightTouch) {	// reverse
 				m_Acceleration = -1f;
 			}
-			else {
+			else {	// normal
 				m_Acceleration = 1f;
 
 				if(leftTouch)
@@ -57,21 +61,21 @@ namespace KartGame.KartSystems
 					m_Steering = 0f;
 			}
 
-			// UNUSED CONTROLS
-			//m_HopHeld = Input.GetKey(KeyCode.Space);
+			m_HopHeld = doubletapHeld;
 
 			if(m_FixedUpdateHappened) {
 				m_FixedUpdateHappened = false;
+				m_HopPressed = false;
 
 				// UNUSED CONTROLS
-				/*m_HopPressed = false;
-				m_BoostPressed = false;
+				/*m_BoostPressed = false;
 				m_FirePressed = false;*/
 			}
 
+			m_HopPressed |= doubletapBegan;
+
 			// UNUSED CONTROLS
-			/*m_HopPressed |= Input.GetKeyDown(KeyCode.Space);
-			m_BoostPressed |= Input.GetKeyDown(KeyCode.RightShift);
+			/*m_BoostPressed |= Input.GetKeyDown(KeyCode.RightShift);
 			m_FirePressed |= Input.GetKeyDown(KeyCode.RightControl);*/
 		}
 
@@ -79,11 +83,20 @@ namespace KartGame.KartSystems
 			m_FixedUpdateHappened = true;
 		}
 
-		private void GetTouchInputs(out bool left, out bool right) {
-			left = right = false;
+		private void GetTouchInputs(out bool left, out bool right, out bool doubletapHeld, out bool doubletapBegan, out float doubletapAxis) {
+			left = right = doubletapHeld = doubletapBegan = false;
+			doubletapAxis = 0f;
 			int halfWidth = Screen.width / 2;
+
 			for(int i = 0; i < Input.touchCount; ++i) {
-				if(!left && Input.GetTouch(i).position.x < halfWidth)
+				if(Input.GetTouch(i).tapCount == 2) {   // doubletap
+					doubletapHeld = true;
+					doubletapBegan = Input.GetTouch(i).phase == TouchPhase.Began;
+					left = right = false;
+					doubletapAxis = (2f * (Input.GetTouch(i).position.x / Screen.width)) - 1f;  // Touch screen X position from -1f to 1f
+					return;
+				}
+				else if(!left && Input.GetTouch(i).position.x < halfWidth)
 					left = true;
 				else if(!right && Input.GetTouch(i).position.x > halfWidth)
 					right = true;
