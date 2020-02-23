d27bcbdfc488d3e057b10eb309c40683c6ab8ba8
José Miguel Tajuelo Garrigós <josemigueltajuelo@gmail.com>
2020-02-23 19:57:42 +0100

    Coin support for main menu UI

--- Assets/Scripts/MainMenuUIScript.cs
+++ Assets/Scripts/MainMenuUIScript.cs
@@ -10,10 +10,11 @@ public class MainMenuUIScript : MonoBehaviour
 		m_PlayButton.onClick.AddListener(delegate { CustomSceneManager.sceneManager.LoadLevel(); });
 		m_ExitButton.onClick.AddListener(delegate { CustomSceneManager.sceneManager.ExitGame(); });
 
-		DisplayRecord();
+		RefreshRecord();
+		RefreshCoins();
 	}
 
-	void DisplayRecord() {
+	void RefreshRecord() {
 		TrackRecord tr = TrackRecord.Load(trackName, 1);
 		if(tr != null && tr.time < float.PositiveInfinity) {
 			m_RecordText.gameObject.SetActive(true);
@@ -22,6 +23,11 @@ public class MainMenuUIScript : MonoBehaviour
 		else {
 			m_RecordText.gameObject.SetActive(false);
 		}
+	}
+
+	void RefreshCoins() {
+		SaveManager.saveManager.Load();
+		m_CoinsText.text = string.Format(m_CoinsText.text, SaveManager.saveManager.earnedCoins);
 	}
 
 	[Tooltip("Reference to the play button.")]
@@ -35,4 +41,7 @@ public class MainMenuUIScript : MonoBehaviour
 
 	[Tooltip("Track to display the record for.")]
 	public string trackName = "ArtTest";
+
+	[Tooltip("Reference to the coins text.")]
+	public Text m_CoinsText;
 }
